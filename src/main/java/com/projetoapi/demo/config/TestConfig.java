32a7e5c976348bf6a338913b8f9c7661ff128892
guiza01 <jefersonguilherme.pe@gmail.com>
2024-08-23 17:58:28 -0300

    Payment, one-to-one association

--- src/main/java/com/projetoapi/demo/config/TestConfig.java
+++ src/main/java/com/projetoapi/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetoapi.demo.entities.Category;
 import com.projetoapi.demo.entities.Order;
 import com.projetoapi.demo.entities.OrderItem;
+import com.projetoapi.demo.entities.Payment;
 import com.projetoapi.demo.entities.Product;
 import com.projetoapi.demo.entities.User;
 import com.projetoapi.demo.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2024-08-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
