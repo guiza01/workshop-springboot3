953a51a35fab7029cbed7363ab960590e30eb431
guiza01 <jefersonguilherme.pe@gmail.com>
2024-08-23 17:47:20 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/projetoapi/demo/repositories/OrderItemRepository.java
+++ src/main/java/com/projetoapi/demo/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.projetoapi.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projetoapi.demo.entities.OrderItem;
+import com.projetoapi.demo.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 	
 }
