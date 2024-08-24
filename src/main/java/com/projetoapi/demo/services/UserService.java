05e4668b85605d1c4f88ca168e361d6460529b84
guiza01 <jefersonguilherme.pe@gmail.com>
2024-08-24 00:32:03 -0300

    Exception handling - update

--- src/main/java/com/projetoapi/demo/services/UserService.java
+++ src/main/java/com/projetoapi/demo/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetoapi.demo.repositories.UserRepository;
 import com.projetoapi.demo.services.exceptions.DatabaseException;
 import com.projetoapi.demo.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -40,13 +42,16 @@ public class UserService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
 	}
 
 	private void updateData(User entity, User obj) {
