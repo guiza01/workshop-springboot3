128a7a206c62d90e438bb6905ae87ab6d394afef
guiza01 <jefersonguilherme.pe@gmail.com>
2024-08-23 23:43:19 -0300

    Exception handling - findById

--- src/main/java/com/projetoapi/demo/entities/Order.java
+++ src/main/java/com/projetoapi/demo/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.projetoapi.demo.entities.enums.OrderStatus;
 
 import jakarta.persistence.CascadeType;
@@ -27,6 +28,8 @@ public class Order implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
 	private Integer orderStatus;
